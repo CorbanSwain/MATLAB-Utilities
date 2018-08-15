a0db46eefc48325d8f09d4694917f8c1f4dcb249
Corban Swain <CorbanSwain@gmail.com>
2018-08-15 12:20:20 -0400

    fixing bug in fftsize function

--- fftsize.m
+++ fftsize.m
@@ -3,7 +3,7 @@ function szout = fftsize(sz1, sz2)
 
 % calculate the minimum extended image size by adding half of the PSF
 % image width and heigth to the image width and heigth, respectively
-minSize = sz1 + min(floor(sz1 / 2), floor(sz2 / 2));
+minSize = max(sz1, sz2) + min(floor(sz1 / 2), floor(sz2 / 2));
 
 % expand the padded dimensions to either a power of two or a multiple
 % of 2^p, choosing whichever is smaller. This to speed up the FFT
